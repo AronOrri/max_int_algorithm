889f33e9e13f077f3d79d256ef33602c3b6a6724
Aron <aronf20@ru.is>
2020-09-03 09:48:55 +0000

    Algorithm written for max_int

---
+++ max_int.py
@@ -0,0 +1,7 @@
+# 1) The user enters an inital number, and changes max to it
+# 2) If the number is not negative then another should be entered otherwise go to step 5
+# 3) Check which of the entered numbers are larger and change the max to the larger one
+# 4) Go back to step 2
+# 5) End the calcutlations
+
+
